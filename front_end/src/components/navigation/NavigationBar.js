425c587fa2d0742c2c0f4f23bb28543924261dfc
lshay <lshay@iastate.edu>
2019-10-16 13:07:02 -0500

    Added styling to navigation bar.

--- front_end/src/components/navigation/NavigationBar.js
+++ front_end/src/components/navigation/NavigationBar.js
@@ -2,15 +2,30 @@ import React from "react"
 import NavigationOption from "./NavigationOption.js"
 import "./NavigationBar.css"
 
-
 const NavigationBar = () => (
     <ul className="navigation-bar">
         <NavigationOption
-            title="Recipes"
-            link="/recipes"/>
+            id="active"
+            align="left"
+            title="Home"
+            link="/Home"/>
         <NavigationOption
-            title="New Recipe"
-            link="/new/recipe"/>
+            align="left"
+            title="Classes"
+            link="/classes"/>
+        <NavigationOption
+            align="left"
+            title="Reminders"
+            link="/reminders"/>
+        <NavigationOption
+            align="left"
+            title="Calendar"
+            link="/calendar"/>
+        <NavigationOption
+            align="right"
+            img="./white-circle.png"
+            title="Account"
+            link="/account"/>
     </ul>
 )
 
--- front_end/src/components/navigation/NavigationOption.js
+++ front_end/src/components/navigation/NavigationOption.js
@@ -1,10 +1,22 @@
 import React from "react"
 import "./NavigationOption.css"
 
-const NavigationOption = (props) => (
-    <li className="navigation-option">
-        <a href={props.link}>{props.title}</a>
-    </li>
-)
+const NavigationOption = (props) => {
+    if (props.img) {
+        return (
+            <li className="img">
+                <img src={props.img} alt={props.img} />
+                <a id={props.id} href={props.link}>{props.title}</a>
+            </li>
+        )
+    }
+    else {
+        return (
+            <li className={props.align}>
+                <a id={props.id} href={props.link}>{props.title}</a>
+            </li>
+        )
+    }
+}
 
 export default NavigationOption
