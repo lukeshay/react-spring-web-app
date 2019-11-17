69a3eb36fe5fc4805f8b76a7a1cc02451db46b23
Luke Shay <shay.luke17@gmail.com>
2019-11-17 15:21:53 -0600

    Trying to fic.

--- front_end/src/App.js
+++ front_end/src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import NavigationBar from "./components/navigation/NavigationBar.js";
 import HomePage from "./components/homepage/HomePage";
 import NotFoundPage from "./components/NotFoundPage";
-import ToDoPage from "./components/todopage/ToDoPage";
+import ToDoPage from "./components/todopage/ToDoPage2";
 import { Route, Switch } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 import "bootstrap/dist/css/bootstrap.min.css";
