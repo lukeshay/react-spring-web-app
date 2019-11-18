bd4c4a0769ea9e35cbf436868da7b4bf596e0850
Luke Shay <shay.luke17@gmail.com>
2019-11-18 12:18:04 -0600

    Switched to hashrouter.

--- front_end/src/index.js
+++ front_end/src/index.js
@@ -1,11 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
-import { BrowserRouter as Router } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 
 ReactDOM.render(
-    <Router>
+    <HashRouter>
         <App />
-    </Router>,
+    </HashRouter>,
     document.getElementById("root")
 );
