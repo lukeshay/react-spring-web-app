9301b9754c0b5ad70199e8ff273d80a30a9fc451
Luke Shay <shay.luke17@gmail.com>
2019-11-18 19:33:07 -0600

    Added a secret password.

--- front_end/src/App.js
+++ front_end/src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import NavigationBar from "./components/navigation/NavigationBar.js";
 import HomePage from "./components/homepage/HomePage";
 import NotFoundPage from "./components/NotFoundPage";
@@ -10,19 +10,33 @@ import "react-toastify/dist/ReactToastify.css";
 import { version } from "../package.json";
 
 function App() {
-    return (
-        <div className="container-fluid">
-            <ToastContainer autoClose={3000} hideProgressBar />
-            <NavigationBar />
-            <Switch>
-                <Route exact path="/" component={HomePage} />
-                <Route exact path="/index" component={HomePage} />
-                <Route path="/todo" component={ToDoPage} />
-                <Route component={NotFoundPage} />
-            </Switch>
-            Version: {version}
-        </div>
-    );
+    const [load, setLoad] = useState("false");
+
+    function handleClick(event) {
+        const enteredName = prompt("Please enter the super secret password");
+
+        setLoad(enteredName === "choochie");
+    }
+
+    if (load === true) {
+        return (
+            <div className="container-fluid">
+                <ToastContainer autoClose={3000} hideProgressBar />
+                <NavigationBar />
+                <Switch>
+                    <Route exact path="/" component={HomePage} />
+                    <Route exact path="/index" component={HomePage} />
+                    <Route path="/todo" component={ToDoPage} />
+                    <Route component={NotFoundPage} />
+                </Switch>
+                Version: {version}
+            </div>
+        );
+    } else {
+        return (
+            <input type="button" value="load web app" onClick={handleClick} />
+        );
+    }
 }
 
 export default App;
