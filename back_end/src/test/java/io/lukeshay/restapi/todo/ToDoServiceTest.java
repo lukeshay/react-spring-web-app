504c7445e7645d30d89dd84c36e7526acf65a792
Luke Shay <shay.luke17@gmail.com>
2019-11-20 09:51:12 -0600

    Added tests.

--- back_end/src/test/java/io/lukeshay/restapi/todo/ToDoServiceTest.java
+++ back_end/src/test/java/io/lukeshay/restapi/todo/ToDoServiceTest.java
@@ -1,4 +1,31 @@
 package io.lukeshay.restapi.todo;
 
-public class TodoServiceTest {
+import com.sun.tools.javac.comp.Todo;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.data.mongo.AutoConfigureDataMongo;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+@AutoConfigureDataMongo
+class ToDoServiceTest {
+
+	@Autowired
+	ToDoService toDoService;
+
+	@Autowired
+	ToDoRepository toDoRepository;
+
+	@Test
+	void addToDoTest() {
+		ToDo addedToDo = new ToDo("id", "text", false);
+		toDoService.saveTodo(addedToDo);
+
+		System.out.println("SOUT " + toDoService.getAllTodos().toString());
+
+		ToDo addedToDo = toDoService.getTodo();
+
+		Assertions.assertAll(() -> Assertions.assertEquals());
+	}
 }
