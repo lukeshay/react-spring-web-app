1b209ce011a4818c1225ebf4d048a0cd95c5aad6
Luke Shay <shay.luke17@gmail.com>
2019-11-25 16:50:09 -0600

    Added cors filter.

--- back_end/src/main/java/io/lukeshay/restapi/RestApiApplication.java
+++ back_end/src/main/java/io/lukeshay/restapi/RestApiApplication.java
@@ -2,7 +2,9 @@ package io.lukeshay.restapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 /**
  * The type Rest api application.
@@ -18,4 +20,15 @@ public class RestApiApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(RestApiApplication.class, args);
 	}
+
+  @Bean
+  public WebMvcConfigurer configurer(){
+    return new WebMvcConfigurer() {
+      @Override
+      public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/*")
+            .allowedOrigins("*");
+      }
+    };
+  }
 }
