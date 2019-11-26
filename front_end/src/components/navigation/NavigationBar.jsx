aed3909f4981c649553cbc53eee3e73ab40e7a53
Luke Shay <shay.luke17@gmail.com>
2019-11-25 18:50:43 -0600

    Removed reminders tab.

--- front_end/src/components/navigation/NavigationBar.jsx
+++ front_end/src/components/navigation/NavigationBar.jsx
@@ -2,32 +2,25 @@ import React from "react";
 import { NavLink } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 
-export default class NavigationBar extends React.Component {
-    render() {
-        return (
-            <nav className="navbar navbar-light bg-dark">
-                <NavLink exact to="/" className="nav-item text-white">
-                    Home
-                </NavLink>
-                <NavLink to="/items" className="nav-item text-white">
-                    Items
-                </NavLink>
-                <NavLink to="/reminders" className="nav-item text-white">
-                    Reminders
-                </NavLink>
-                <NavLink to="/todo" className="nav-item text-white">
-                    ToDo
-                </NavLink>
-                <NavLink to="/calendar" className="nav-item text-white">
-                    Calendar
-                </NavLink>
-                <NavLink to="/email" className="nav-item text-white">
-                    Email
-                </NavLink>
-                <NavLink to="/account" className="nav-item text-white">
-                    Account
-                </NavLink>
-            </nav>
-        );
-    }
-}
+const NavigationBar = () => (
+    <nav className="navbar navbar-light bg-dark">
+        <NavLink exact to="/" className="nav-item text-white">
+            Home
+        </NavLink>
+        <NavLink to="/items" className="nav-item text-white">
+            Items
+        </NavLink>
+        <NavLink to="/todo" className="nav-item text-white">
+            ToDo
+        </NavLink>
+        <NavLink to="/calendar" className="nav-item text-white">
+            Calendar
+        </NavLink>
+        <NavLink to="/email" className="nav-item text-white">
+            Email
+        </NavLink>
+        <NavLink to="/account" className="nav-item text-white">
+            Account
+        </NavLink>
+    </nav>
+);
