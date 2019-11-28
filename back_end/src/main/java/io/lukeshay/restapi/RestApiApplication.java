63bbca117b5e7761f21564d9aadf8e5af9f08861
Luke Shay <shay.luke17@gmail.com>
2019-11-27 22:19:26 -0600

    Moved cors to configuration.

--- back_end/src/main/java/io/lukeshay/restapi/RestApiApplication.java
+++ back_end/src/main/java/io/lukeshay/restapi/RestApiApplication.java
@@ -2,9 +2,6 @@ package io.lukeshay.restapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 /**
  * The type Rest api application.
@@ -19,18 +16,5 @@ public class RestApiApplication {
 	 */
 	public static void main(String[] args) {
 		SpringApplication.run(RestApiApplication.class, args);
-	}
-
-	@Bean
-	public WebMvcConfigurer configurer() {
-		return new WebMvcConfigurer() {
-			@Override
-			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/*")
-						.allowedMethods("GET", "POST", "PUT", "DELETE")
-						.allowedOrigins("*")
-						.allowedHeaders("*");
-			}
-		};
 	}
 }
