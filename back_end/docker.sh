d90e79d633a9c3ce888c2381a40a48328ffb673a
Luke Shay <shay.luke17@gmail.com>
2019-11-28 00:09:41 -0600

    Did some werk.

--- back_end/docker.sh
+++ back_end/docker.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
 ./gradlew clean build
-docker ps | awk 'NR != 1{print $1}' | xargs docker stop
-docker images | awk 'NR != 1{print $3}' | xargs docker rmi
-docker system prune -f
+docker-compose down --rmi all
 docker-compose up -d
---
+++ back_end/src/main/java/io/lukeshay/restapi/SecurityConfiguration.java
@@ -0,0 +1,31 @@
+package io.lukeshay.restapi;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.cors().disable()
+				.authorizeRequests()
+				.antMatchers(HttpMethod.OPTIONS, "/**").permitAll()
+				.anyRequest()
+				.fullyAuthenticated()
+				.and()
+				.httpBasic()
+				.and()
+				.csrf().disable();
+	}
+
+	@Override
+	public void configure(WebSecurity web) {
+		web.ignoring().antMatchers(HttpMethod.OPTIONS, "/**");
+	}
+}
