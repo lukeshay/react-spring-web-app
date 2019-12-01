3c3db71c43fc7d55cea54509ac8f5b86b7b135bb
Luke Shay <shay.luke17@gmail.com>
2019-12-01 17:38:09 -0600

    Changed some stuff.

--- back_end/src/main/java/io/lukeshay/restapi/todo/ToDo.java
+++ back_end/src/main/java/io/lukeshay/restapi/todo/ToDo.java
@@ -7,6 +7,7 @@ import lombok.Setter;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.util.Date;
 
@@ -16,6 +17,7 @@ import java.util.Date;
 @Getter
 @Setter
 @NoArgsConstructor
+@Document
 public class ToDo {
 	@Id
 	private String id;
--- back_end/src/main/java/io/lukeshay/restapi/user/User.java
+++ back_end/src/main/java/io/lukeshay/restapi/user/User.java
@@ -1,10 +1,22 @@
 package io.lukeshay.restapi.user;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.mongodb.core.mapping.Document;
 
 /**
  * The type User.
  */
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Document
 public class User {
 	@Id
 	private String userId;
@@ -16,168 +28,9 @@ public class User {
 	private String state;
 	private String country;
 
-	/**
-	 * Instantiates a new User.
-	 *
-	 * @param firstName   the first name
-	 * @param lastName    the last name
-	 * @param userName    the user name
-	 * @param email       the email
-	 * @param phoneNumber the phone number
-	 * @param state       the state
-	 * @param country     the country
-	 */
-	public User(String firstName, String lastName, String userName, String email, String phoneNumber, String state, String country) {
-		this.firstName = firstName;
-		this.lastName = lastName;
-		this.userName = userName;
-		this.email = email;
-		this.phoneNumber = phoneNumber;
-		this.state = state;
-		this.country = country;
-	}
+	@CreatedDate
+	private String createdDate;
 
-	/**
-	 * Gets user id.
-	 *
-	 * @return the user id
-	 */
-	public String getUserId() {
-		return userId;
-	}
-
-	/**
-	 * Sets user id.
-	 *
-	 * @param userId the user id
-	 */
-	public void setUserId(String userId) {
-		this.userId = userId;
-	}
-
-	/**
-	 * Gets first name.
-	 *
-	 * @return the first name
-	 */
-	public String getFirstName() {
-		return firstName;
-	}
-
-	/**
-	 * Sets first name.
-	 *
-	 * @param firstName the first name
-	 */
-	public void setFirstName(String firstName) {
-		this.firstName = firstName;
-	}
-
-	/**
-	 * Gets last name.
-	 *
-	 * @return the last name
-	 */
-	public String getLastName() {
-		return lastName;
-	}
-
-	/**
-	 * Sets last name.
-	 *
-	 * @param lastName the last name
-	 */
-	public void setLastName(String lastName) {
-		this.lastName = lastName;
-	}
-
-	/**
-	 * Gets user name.
-	 *
-	 * @return the user name
-	 */
-	public String getUserName() {
-		return userName;
-	}
-
-	/**
-	 * Sets user name.
-	 *
-	 * @param userName the user name
-	 */
-	public void setUserName(String userName) {
-		this.userName = userName;
-	}
-
-	/**
-	 * Gets email.
-	 *
-	 * @return the email
-	 */
-	public String getEmail() {
-		return email;
-	}
-
-	/**
-	 * Sets email.
-	 *
-	 * @param email the email
-	 */
-	public void setEmail(String email) {
-		this.email = email;
-	}
-
-	/**
-	 * Gets phone number.
-	 *
-	 * @return the phone number
-	 */
-	public String getPhoneNumber() {
-		return phoneNumber;
-	}
-
-	/**
-	 * Sets phone number.
-	 *
-	 * @param phoneNumber the phone number
-	 */
-	public void setPhoneNumber(String phoneNumber) {
-		this.phoneNumber = phoneNumber;
-	}
-
-	/**
-	 * Gets state.
-	 *
-	 * @return the state
-	 */
-	public String getState() {
-		return state;
-	}
-
-	/**
-	 * Sets state.
-	 *
-	 * @param state the state
-	 */
-	public void setState(String state) {
-		this.state = state;
-	}
-
-	/**
-	 * Gets country.
-	 *
-	 * @return the country
-	 */
-	public String getCountry() {
-		return country;
-	}
-
-	/**
-	 * Sets country.
-	 *
-	 * @param country the country
-	 */
-	public void setCountry(String country) {
-		this.country = country;
-	}
+	@LastModifiedDate
+	private String modifiedDate;
 }
