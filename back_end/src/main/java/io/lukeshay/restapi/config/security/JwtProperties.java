8411726796d83d28ea2c92f88821972eda1c402e
Luke Shay <shay.luke17@gmail.com>
2019-12-07 15:37:16 -0600

    Jwt secret is now generated prior to build.:

--- back_end/src/main/java/io/lukeshay/restapi/config/security/JwtProperties.java
+++ back_end/src/main/java/io/lukeshay/restapi/config/security/JwtProperties.java
@@ -1,7 +1,7 @@
 package io.lukeshay.restapi.config.security;
 
 class JwtProperties {
-    static final String SECRET = "SomeSecretForJWTGeneration";
+    static final String SECRET = System.getenv("JWT_SECRET");
     static final int EXPIRATION_TIME = 864_000_000; // 10 days
     static final String TOKEN_PREFIX = "Bearer ";
     static final String HEADER_STRING = "Authorization";
