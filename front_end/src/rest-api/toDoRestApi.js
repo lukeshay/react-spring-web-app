72ba1bc88b2b1de4c507e13aa74914049aeb341c
Luke Shay <shay.luke17@gmail.com>
2019-12-15 11:07:21 -0600

    Modified to-do calls so error handling can be in the component.

--- front_end/src/rest-api/toDoRestApi.js
+++ front_end/src/rest-api/toDoRestApi.js
@@ -1,41 +1,31 @@
 import { handleError } from "./apiUtils";
-import { toast } from "react-toastify";
-import { getJwtToken } from "../utils/cookiesUtils";
+import * as Cookies from "../utils/cookiesUtils";
 
 const baseUrl = process.env.BASE_URL + "todos/";
 
 export function getUsersToDos(userId) {
   return fetch(baseUrl + userId, {
     headers: {
-      Authorization: getJwtToken()
+      Authorization: Cookies.getJwtToken()
     }
   })
     .then(response => {
-      if (response.ok) {
-        return response.json();
-      } else {
-        toast.error("There was an error getting your todos.");
-      }
+      return response;
     })
     .catch(handleError);
 }
 
 export function saveToDo(toDo) {
   return fetch(baseUrl + (toDo.id || ""), {
-    method: toDo.id ? "PUT" : "POST", // POST for create, PUT to update when id already exists.
+    method: toDo.id ? "PUT" : "POST",
     headers: {
       "Content-Type": "application/json",
-      Authorization: getJwtToken(),
-      Origin: ""
+      Authorization: Cookies.getJwtToken()
     },
     body: JSON.stringify(toDo)
   })
     .then(response => {
-      if (response.ok) {
-        return response.json();
-      } else {
-        toast.error("There was an error saving your to-do.");
-      }
+      return response;
     })
     .catch(handleError);
 }
@@ -43,14 +33,10 @@ export function saveToDo(toDo) {
 export function deleteToDo(toDoId) {
   return fetch(baseUrl + toDoId, {
     method: "DELETE",
-    headers: { Authorization: getJwtToken() }
+    headers: { Authorization: Cookies.getJwtToken() }
   })
     .then(response => {
-      if (response.ok) {
-        return response.json();
-      } else {
-        toast.error("There was an error deleting your todo.");
-      }
+      return response;
     })
     .catch(handleError);
 }
