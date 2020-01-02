072e542cd24279f59f37096d866fb4237bc5f768
Luke Shay <shay.luke17@gmail.com>
2020-01-01 20:17:07 -0600

    Added endpoint to add walls.

--- back_end/src/main/java/com/lukeshay/restapi/wall/WallController.java
+++ back_end/src/main/java/com/lukeshay/restapi/wall/WallController.java
@@ -1,22 +1,46 @@
 package com.lukeshay.restapi.wall;
 
+import com.lukeshay.restapi.utils.Bodys;
+import com.lukeshay.restapi.utils.Responses;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import javax.servlet.http.HttpServletRequest;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/wall")
 @PreAuthorize("isAuthenticated()")
+@Api(value = "Wall api endpoints.")
 public class WallController {
   private static Logger LOG = LoggerFactory.getLogger(WallController.class.getName());
 
-  private WallRepository wallRepository;
+  private WallService wallService;
 
   @Autowired
-  public WallController(WallRepository wallRepository) {
-    this.wallRepository = wallRepository;
+  public WallController(WallService wallService) {
+    this.wallService = wallService;
+  }
+
+  @PostMapping("")
+  @PreAuthorize("isAuthenticated()")
+  @ApiOperation(value = "Create a new wall", response = Wall.class)
+  public ResponseEntity<?> createWall(HttpServletRequest request, @RequestBody Wall body) {
+    LOG.debug("Adding wall {}", body);
+
+    Wall wall = wallService.createWall(request, body);
+
+    if (wall == null) {
+      return Responses.badRequestJsonResponse(Bodys.error("Error adding wall"));
+    } else {
+      return Responses.okJsonResponse(wall);
+    }
   }
 }
--- back_end/src/main/java/com/lukeshay/restapi/wall/WallService.java
+++ back_end/src/main/java/com/lukeshay/restapi/wall/WallService.java
@@ -1,3 +1,40 @@
 package com.lukeshay.restapi.wall;
 
-public class WallService {}
+import com.lukeshay.restapi.gym.Gym;
+import com.lukeshay.restapi.gym.GymRepository;
+import com.lukeshay.restapi.services.Requests;
+import com.lukeshay.restapi.user.User;
+import javax.servlet.http.HttpServletRequest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class WallService {
+
+  private WallRepository wallRepository;
+  private GymRepository gymRepository;
+  private Requests requests;
+
+  @Autowired
+  public WallService(WallRepository wallRepository, GymRepository gymRepository, Requests requests) {
+    this.wallRepository = wallRepository;
+    this.gymRepository = gymRepository;
+    this.requests = requests;
+  }
+
+  public Wall createWall(HttpServletRequest request, Wall body) {
+    User user = requests.getUserFromRequest(request);
+
+    if (user == null) {
+      return null;
+    }
+
+    Gym gym = gymRepository.findById(body.getGymId()).orElse(null);
+
+    if (gym == null || !gym.getAuthorizedEditors().contains(user.getUserId())) {
+      return null;
+    }
+
+    return wallRepository.save(body);
+  }
+}
