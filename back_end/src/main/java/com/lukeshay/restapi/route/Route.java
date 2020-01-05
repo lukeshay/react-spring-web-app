824be738dc1594d0d88bfeae03d57c5b07153a6b
Luke Shay <shay.luke17@gmail.com>
2020-01-05 10:44:02 -0700

    Updated route.

--- back_end/src/main/java/com/lukeshay/restapi/route/Route.java
+++ back_end/src/main/java/com/lukeshay/restapi/route/Route.java
@@ -1,21 +1,49 @@
 package com.lukeshay.restapi.route;
 
+import com.google.gson.GsonBuilder;
+import com.google.gson.annotations.Expose;
+import com.lukeshay.restapi.route.RouteProperties.Grade;
+import com.lukeshay.restapi.user.User;
+import com.lukeshay.restapi.wall.WallProperties.WallTypes;
 import java.util.List;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.domain.Persistable;
 
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
-class Route {
-  private String name;
-  private String setter;
-  private List<String> types;
-  private List<Grade> userGrade;
-  private Grade averageGrade;
-  private List<Integer> userRating;
-  private Integer averageRating;
+class Route implements Persistable<String> {
+  @Id @Expose String id;
+
+  @Expose String wallId;
+  @Expose String gymId;
+
+  @Expose private String name;
+  @Expose private String setter;
+  @Expose private List<WallTypes> types;
+  @Expose private List<Grade> userGrade;
+  @Expose private Grade averageGrade;
+  @Expose private List<Integer> userRating;
+  @Expose private Integer averageRating;
+  private boolean persistable;
+
+  @Override
+  public boolean isNew() {
+    return !persistable;
+  }
+
+  @Override
+  public boolean equals(Object obj) {
+    return obj instanceof User && toString().equals(obj.toString());
+  }
+
+  @Override
+  public String toString() {
+    return new GsonBuilder().excludeFieldsWithoutExposeAnnotation().create().toJson(this);
+  }
 }
