436f00900f0df1b3d07475f2d4a2fed64461c11b
Luke Shay <shay.luke17@gmail.com>
2020-01-11 10:35:43 -0700

    Deleted old files.

--- front_end/src/modules/gyms/GymsRouter.tsx
+++ front_end/src/modules/gyms/GymsRouter.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { Route, Switch } from "react-router";
 import { AuthRoutes, Routes } from "../../routes";
 
-const GymsPage = React.lazy(() => import("./views/GymsList"));
+const GymsPage = React.lazy(() => import("./views/GymsPage"));
 const GymPage = React.lazy(() => import("./views/GymPage"));
 const GymEditPage = React.lazy(() => import("./views/GymEditPage"));
 
