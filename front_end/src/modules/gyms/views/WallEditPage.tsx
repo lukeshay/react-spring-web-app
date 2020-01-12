c02246a38693b47275bbac8bf3ae262420eb38af
Luke Shay <shay.luke17@gmail.com>
2020-01-11 22:29:54 -0600

    Added logic to retrieve the wall.:

--- front_end/src/modules/gyms/views/WallEditPage.tsx
+++ front_end/src/modules/gyms/views/WallEditPage.tsx
@@ -1,9 +1,62 @@
-import * as React from "react";
+import React from "react";
+import * as ReactRouter from "react-router";
+import { useGymsContext } from "../../../context/gyms/gymsStore";
+import { useUserContext } from "../../../context/user/userStore";
+import { Routes } from "../../../routes";
+import { Gym, Wall } from "../../../types";
 
-interface IWallEditPageProps {}
+const WallEditPage: React.FunctionComponent = () => {
+  const history = ReactRouter.useHistory();
 
-const WallEditPage: React.FunctionComponent<IWallEditPageProps> = (props) => {
-  return;
+  const [wall, setWall] = React.useState<Wall>({} as Wall);
+  const [gym, setGym] = React.useState<Gym>({} as Gym);
+
+  const { state: gymsState } = useGymsContext();
+  const { state: userState } = useUserContext();
+
+  React.useEffect(() => {
+    const { user } = userState;
+
+    const wallId = history.location.pathname
+      .split("/")
+      .splice(-1)
+      .pop();
+
+    const tempGym = gymsState.gyms
+      .filter(
+        (element) =>
+          element.walls &&
+          element.walls.filter((elementWall) => elementWall.id === wallId)
+            .length > 0
+      )
+      .pop();
+
+    if (
+      tempGym &&
+      user &&
+      tempGym.authorizedEditors &&
+      tempGym.authorizedEditors.find(
+        (editorId: string) => editorId === user.userId
+      ) &&
+      tempGym.walls
+    ) {
+      const tempWall = tempGym.walls
+        .filter((element) => element.id === wallId)
+        .pop();
+      setGym(tempGym);
+
+      // Only here to silence warning
+      if (tempWall) {
+        setWall(tempWall);
+      }
+    } else if (tempGym) {
+      history.push(Routes.GYMS + "/" + tempGym.id);
+    } else {
+      history.push(Routes.GYMS);
+    }
+  }, []);
+
+  return <div>{wall}</div>;
 };
 
 export default WallEditPage;
