649a629fb5355ceb5bfe185ca0185fecb6cf3c06
Luke Shay <shay.luke17@gmail.com>
2020-01-12 22:07:22 -0600

    Created wall api functions.

--- front_end/src/api/routesApi.ts
+++ front_end/src/api/routesApi.ts
@@ -1,9 +1,62 @@
 import { handleError } from "./apiUtils";
+import { Route } from "../types";
+import * as Cookies from "../utils/cookiesUtils";
 
 const baseUrl = process.env.BASE_URL + "routes";
 
 export const getRoutesOfWall = async (wallId: string) => {
   return fetch(baseUrl + "/" + wallId)
+    .then(
+      (response: Response): Response => {
+        return response;
+      }
+    )
+    .catch(handleError);
+};
+
+export const createRoute = async (route: Route): Promise<void | Response> => {
+  return fetch(baseUrl, {
+    body: JSON.stringify(route),
+    headers: {
+      Authorization: Cookies.getJwtToken(),
+      "Content-Type": "application/json"
+    },
+    method: "POST"
+  })
+    .then(
+      (response: Response): Response => {
+        return response;
+      }
+    )
+    .catch(handleError);
+};
+
+export const updateRoute = async (route: Route): Promise<void | Response> => {
+  return fetch(baseUrl, {
+    body: JSON.stringify(route),
+    headers: {
+      Authorization: Cookies.getJwtToken(),
+      "Content-Type": "application/json"
+    },
+    method: "PUT"
+  })
+    .then(
+      (response: Response): Response => {
+        return response;
+      }
+    )
+    .catch(handleError);
+};
+
+export const deleteRoute = async (route: Route): Promise<void | Response> => {
+  return fetch(baseUrl, {
+    body: JSON.stringify(route),
+    headers: {
+      Authorization: Cookies.getJwtToken(),
+      "Content-Type": "application/json"
+    },
+    method: "DELETE"
+  })
     .then(
       (response: Response): Response => {
         return response;
