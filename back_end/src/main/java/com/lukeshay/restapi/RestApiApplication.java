22f9e41ea1a0246bb0091ee4f2ba3ea588da3df3
Luke Shay <shay.luke17@gmail.com>
2020-01-23 17:29:11 -0600

    More.

--- back_end/src/main/java/com/lukeshay/restapi/RestApiApplication.java
+++ back_end/src/main/java/com/lukeshay/restapi/RestApiApplication.java
@@ -3,10 +3,12 @@ package com.lukeshay.restapi;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.mongodb.config.EnableMongoAuditing;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 
 /** The type Rest api application. */
 @SpringBootApplication
 @EnableMongoAuditing
+@EnableAuthorizationServer
 public class RestApiApplication {
 
   /**
--- back_end/src/main/java/com/lukeshay/restapi/config/security/MyUserDetailsService.java
+++ back_end/src/main/java/com/lukeshay/restapi/config/security/MyUserDetailsService.java
@@ -1,7 +1,6 @@
 package com.lukeshay.restapi.config.security;
 
 import com.lukeshay.restapi.user.UserRepository;
-import com.lukeshay.restapi.utils.Exceptions;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -18,22 +17,8 @@ public class MyUserDetailsService implements UserDetailsService {
     this.userRepository = userRepository;
   }
 
-  /**
-   * Locates the user based on the username. In the actual implementation, the search may possibly
-   * be case sensitive, or case insensitive depending on how the implementation instance is
-   * configured. In this case, the <code>UserDetails</code> object that comes back may have a
-   * username that is of a different case than what was actually requested..
-   *
-   * @param username the username identifying the user whose data is required.
-   * @return a fully populated user record (never <code>null</code>)
-   * @throws UsernameNotFoundException if the user could not be found or the user has no
-   *     GrantedAuthority
-   */
   @Override
   public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-    return new MyUserDetails(
-        userRepository
-            .findByUsername(username)
-            .orElseThrow(() -> Exceptions.notFound(String.format("%s not found.", username))));
+    return new MyUserDetails(userRepository.findByUsername(username).orElse(null));
   }
 }
