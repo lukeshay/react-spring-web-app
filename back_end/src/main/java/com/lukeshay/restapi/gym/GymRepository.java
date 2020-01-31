9a2275ea761a9cea0f6b7abc82a424e739a3db62
Luke Shay <shay.luke17@gmail.com>
2020-01-30 18:30:49 -0600

    I finished basic method.

--- back_end/src/main/java/com/lukeshay/restapi/gym/GymRepository.java
+++ back_end/src/main/java/com/lukeshay/restapi/gym/GymRepository.java
@@ -1,7 +1,12 @@
 package com.lukeshay.restapi.gym;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface GymRepository extends JpaRepository<Gym, String> {}
+public interface GymRepository extends JpaRepository<Gym, String> {
+
+  Page<Gym> findAllByNameContaining(Pageable pageable, String query);
+}
