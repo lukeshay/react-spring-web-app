dbd32bc510e6bfb29ac66ee94477dcb72e7e233b
Luke Shay <shay.luke17@gmail.com>
2020-02-01 14:18:07 -0600

    Added new endpoint support.

--- front_end/src/types/index.ts
+++ front_end/src/types/index.ts
@@ -7,3 +7,12 @@ export { ButtonEvent, InputEvent } from "./events";
 export { RouteRatingJwt } from "./routeRatingJwt";
 export { Session } from "./session";
 export { AuthBody } from "./authBody";
+export {
+  Sort,
+  Pageable,
+  GymPage,
+  Page,
+  RoutePage,
+  UserPage,
+  WallPage
+} from "./page";
