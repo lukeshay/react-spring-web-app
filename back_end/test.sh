868e58524e21d301fbfa807ff84255fd95d75d66
Luke Shay <shay.luke17@gmail.com>
2020-02-03 19:50:20 -0600

    More.

--- back_end/test.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-docker run \
-  -e JWT_SECRET=$JWT_SECRET \
-  -e REFRESH_SECRET=$REFRESH_SECRET \
-  -e ACCESS_KEY=$ACCESS_KEY \
-  -e SECRET_KEY=$SECRET_KEY \
-  --entrypoint ./scripts/test.sh \
-  rest-api:latest || exit 1
